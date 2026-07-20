32c54b9c397743a136e15c2528cb388b8fcb87db
AaronDcunha <60778473+AaronDcunha@users.noreply.github.com>
2026-07-20 14:33:16 -0500

    removed secret

--- backend/testinggemini.py
+++ backend/testinggemini.py
@@ -1,7 +1,7 @@
 import json
 import google.generativeai as genai
 
-genai.configure(api_key="AIzaSyCmRlApa14b-NWXDakx9iqlle9dzWjXHbc")
+genai.configure(api_key="")
 
 model = genai.GenerativeModel("gemini-2.0-flash")
 
